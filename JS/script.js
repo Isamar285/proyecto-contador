b8b5a167c3b6ff5d97240b91117ec4ff2e337e77
jenny <jennyisamar.7@gmail.com>
2026-04-09 23:00:35 -0600

    feature: agregado boton de reset

--- JS/script.js
+++ JS/script.js
@@ -3,6 +3,12 @@ let contador = 0; // creamos una variable para llevar la cuenta
 const spanNumero= document.getElementById("numero"); // seleccionamos elementos de html usando sus ides  
 
 const btnAumentar = document.getElementById("btn-aumentar");
+const btnReset = document.getElementById("btn-reset");
+
+btnReset.addEventListener("click", () => {
+    contador = 0;
+    spanNumero.innerText = contador;
+});
 
 btnAumentar.addEventListener("click", () => {     //escuchamos cuando alguien haga click en el boton 
     contador++;   //aumentamos el valor en 1
